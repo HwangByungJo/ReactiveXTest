6975f1c9704932d3f729ed27eee23eb2b042f683
HwangByungJo <iDeveloper.kr@gmail.com>
2018-05-03 17:58:48 +0900

    pod update

--- ReactiveXTest/ViewController.swift
+++ ReactiveXTest/ViewController.swift
@@ -6,14 +6,48 @@
 //  Copyright © 2018년 iDeveloper. All rights reserved.
 //
 
+import RxSwift
+import RxCocoa
 import UIKit
 
 class ViewController: UIViewController {
 
+    var disposeBag: DisposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        checkArrayObservable(items: [4, 3, 0, 5, 2])
+            .subscribe{ event in
+                switch event {
+                    case .next(let value) :
+                        print(value)
+                    case .error(let error) :
+                        print(error)
+                    case .completed:
+                    print("complete")
+                }
+        }.disposed(by: self.disposeBag)
     }
+    
+    func checkArrayObservable(items: [Int]) -> Observable<Int> {
+        return Observable<Int>.create { observer -> Disposable in
+        
+            for item in items{
+                if item == 0 {
+                    observer.onError(NSError(domain: "ERROR: value is zero", code: 0, userInfo: nil))
+                    break
+                }
+                observer.onNext(item)
+                sleep(1)
+            }
+        observer.onCompleted()
+        return Disposables.create()
+        }
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
